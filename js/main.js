4e5cbf37b0669bca6b84367c43b9900e06c78475
tulamor <romano@linuxmail.org>
2020-03-06 17:47:35 +0100

    final version

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 
-// Search states.json and filter it
-const searchStates = async searchText => {
+const searchPackage = async searchText => {
   const res = await fetch('../data/data.json');
-  const states = await res.json();
+  const packages = await res.json();
 
-  //Get matches to current text input
-  let matches = states.filter(state => {
+// Get matches to current text input
+  let matches = packages.filter(package => {
       const regex = new RegExp(`^${searchText}`, 'gi');
-      return state.name.match(regex) || state.package_type.match(regex);
+      return package.name.match(regex) || package.package_type.match(regex);
   });
-
-
-  // "hashtag": "dd894912571b8cd046255b897c5190d7", 
-  // "name": "OpenBLAS-toolfile", 
-  // "package_type": "external", 
-  // "ver_suffix": "1.0-bcolbf"
 
   if (searchText.lenght === 0) {
     mathes = [];
@@ -43,5 +36,5 @@ const outputHtml = matches => {
   }
 };
 
-search.addEventListener('input', () => searchStates(search.value))
+search.addEventListener('input', () => searchPackage(search.value))
 ;
